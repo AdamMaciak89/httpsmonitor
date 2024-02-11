ba4bccf68ee637b80b2a4af04d079530baadc6d1
AdamMaciak89 <157085329+AdamMaciak89@users.noreply.github.com>
2024-02-11 03:14:12 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import checker.WebsiteAvailabilityChecker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Uruchomienie aplikacji do sprawdzania dostępności stron
+        WebsiteAvailabilityChecker.main(args);
+    }
+}
